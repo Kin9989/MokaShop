65776fc88e690d7734bbefd4f4f8c25b53466139
Kin9989 <HaoQH@int.t4tek.co>
2023-07-25 20:06:51 +0700

    update

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,3 +1,3 @@
 // const baseURL = "http://localhost:8080/api/v1";
-const baseURL = "https://ecommerce-api-mokashop.onrender.com/api/v1";
+const baseURL = "https://ApiMokaShop.onrender.com/api/v1";
 export default baseURL;
